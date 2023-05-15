90eec68c67baa11a0efc06a2febf87eb11587936
ANDQE <2538634015@qq.com>
2023-05-15 20:40:56 +0800

    利用互斥锁解决缓存击穿问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -31,23 +31,90 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryById(Long id) {
-        String key=CACHE_SHOP_KEY+id;
-        String shopJson = stringRedisTemplate.opsForValue().get(key);
-        if (shopJson!=null){
-            if (StrUtil.isNotBlank(shopJson)) {
-                Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-                return Result.ok(shop);
-            }
-            return Result.ok(shopJson);
-        }
-        Shop shop = getById(id);
-        if (shop==null){
-            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
-            return Result.fail("店铺不存在");
-        }
-        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        return Result.ok(shop);
+        return Result.ok(queryWithMutex(id));
     }
+
+    /**
+     * @Param id: 
+     * @return: com.hmdp.dto.Result
+     * description: 缓存null值解决商品查询的缓存穿透问题
+     */
+//    @Override
+//    public Result queryById(Long id) {
+//        String key=CACHE_SHOP_KEY+id;
+//        String shopJson = stringRedisTemplate.opsForValue().get(key);
+//        //从redis查询缓存命中
+//        if (shopJson!=null){
+//            if (StrUtil.isNotBlank(shopJson)) {
+//                Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+//                return Result.ok(shop);
+//            }
+//            return Result.ok(shopJson);
+//        }
+//        Shop shop = getById(id);
+//        if (shop==null){
+//            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
+//            return Result.fail("店铺不存在");
+//        }
+//        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
+//        return Result.ok(shop);
+//    }
+
+    /**
+     * @Param id:
+     * @return: com.hmdp.entity.Shop
+     * description: 利用互斥锁解决缓存击穿问题
+     */
+    public Shop queryWithMutex(Long id)  {
+        String key = CACHE_SHOP_KEY + id;
+        // 1、从redis中查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get("key");
+        // 2、判断是否存在
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 存在,直接返回
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+        //判断命中的值是否是空值
+        if (shopJson != null) {
+            //返回一个错误信息
+            return null;
+        }
+        // 4.实现缓存重构
+        //4.1 获取互斥锁
+        String lockKey = "lock:shop:" + id;
+        Shop shop = null;
+        try {
+            boolean isLock = tryLock(lockKey);
+            // 4.2 判断否获取成功
+            if(!isLock){
+                //4.3 失败，则休眠重试
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+            //4.4 成功，根据id查询数据库
+            shop = getById(id);
+            // 5.不存在，返回错误
+            if(shop == null){
+                //将空值写入redis
+                stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
+                //返回错误信息
+                return null;
+            }
+            //6.写入redis
+            stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_NULL_TTL,TimeUnit.MINUTES);
+
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        finally {
+            //7.释放互斥锁
+            unLock(lockKey);
+        }
+        return shop;
+    }
+
+
+
 
     @Override
     public Result update(Shop shop) {
@@ -60,5 +127,13 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         //2.删除缓存
         stringRedisTemplate.delete(CACHE_SHOP_KEY+id);
         return Result.ok();
+    }
+
+    private boolean tryLock(String key){
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", LOCK_SHOP_TTL, TimeUnit.SECONDS);
+        return flag;
+    }
+    private void unLock(String key){
+        stringRedisTemplate.delete(key);
     }
 }
